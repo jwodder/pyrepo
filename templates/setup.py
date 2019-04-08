cb3a96b2884de7247c21dd272b3876b538e519d6
John T. Wodder II <git@varonathe.org>
2019-04-08 14:15:30 +0000

    Set file encoding when reading __version__
    This prevents builds from failing in non-UTF-8 environments.

--- templates/setup.py
+++ templates/setup.py
@@ -2,14 +2,17 @@
 from setuptools import setup
 setup()
 {% else %}
+{% if py2 %}
+import io
+{% endif %}
 from   os.path    import dirname, join
 import re
 from   setuptools import setup
 
 {% if is_flat_module %}
-with open(join(dirname(__file__), {{(import_name + '.py')|repr}})) as fp:
+with {% if py2 %}io.{% endif %}open(join(dirname(__file__), {{(import_name + '.py')|repr}}), encoding='utf-8') as fp:
 {% else %}
-with open(join(dirname(__file__), {{import_name|repr}}, '__init__.py')) as fp:
+with {% if py2 %}io.{% endif %}open(join(dirname(__file__), {{import_name|repr}}, '__init__.py'), encoding='utf-8') as fp:
 {% endif %}
     for line in fp:
         m = re.search(r'^\s*__version__\s*=\s*([\'"])([^\'"]+)\1\s*$', line)
