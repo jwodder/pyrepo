9f4b710904a6aab25a8fc2fc91e551686709af50
John T. Wodder II <git@varonathe.org>
2019-04-15 16:23:56 +0000

    Added a `--version` option

--- pyrepo/__main__.py
+++ pyrepo/__main__.py
@@ -1,8 +1,12 @@
 import click
+from   .        import __version__
 from   .init    import init
 from   .release import release
 
 @click.group(context_settings={"help_option_names": ["-h", "--help"]})
+@click.version_option(
+    __version__, '-V', '--version', message='%(prog)s %(version)s',
+)
 def main():
     pass
 
