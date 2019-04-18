4912d71ea676b245ce5c18b971331901c3273ebb
John T. Wodder II <git@varonathe.org>
2019-04-18 18:16:22 +0000

    Tests for getting requirements from __requires__

---
+++ test/data/pyrepo_init/requires-flat/before/foobar.py
@@ -0,0 +1,22 @@
+__requires__ = [
+    'attrs ~= 19.1',
+    'click ~= 7.0',
+]
+
+from collections import Counter
+
+def life(before):
+    """
+    Takes as input a state of Conway's Game of Life, represented as an iterable
+    of ``(int, int)`` pairs giving the coordinates of living cells, and returns
+    a `set` of ``(int, int)`` pairs representing the next state
+    """
+    before = set(before)
+    neighbors = Counter(
+        (x+i, y+j) for (x,y) in before
+                   for i in [-1,0,1]
+                   for j in [-1,0,1]
+                   if (i,j) != (0,0)
+    )
+    return {xy for (xy, n) in neighbors.items()
+               if n == 3 or (n == 2 and xy in before)}
