ac8c40d7ec1135f30387e6f713a5ca409b3c7d35
John T. Wodder II <git@varonathe.org>
2019-05-02 21:43:44 +0000

    Tests for README parser

---
+++ test/test_readme.py
@@ -0,0 +1,15 @@
+import pytest
+from   pyrepo.readme import Readme
+
+DATA_DIR = Path(__file__).with_name('data')
+
+@pytest.mark.parametrize('filepath', [
+    p for p in (DATA_DIR / 'readme').iterdir()
+      if p.suffix == '.rst'
+], ids=attrgetter("name"))
+def test_readme(filepath):
+    with filepath.open(encoding='utf-8') as fp:
+        rme = Readme.parse(fp)
+    assert rme.for_json() \
+        == json.loads(filepath.with_suffix('.json').read_text())
+    assert str(rme) == filepath.read_text(encoding='utf-8')
