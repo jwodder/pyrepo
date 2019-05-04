09c762d8b09d1d611adb25878002d5f53a069be3
John T. Wodder II <git@varonathe.org>
2019-05-04 19:20:04 +0000

    is_flat() is no longer used; delete it.

--- pyrepo/inspect_project.py
+++ pyrepo/inspect_project.py
@@ -113,20 +113,6 @@ def inspect_project(dirpath=None):
 
     return env
 
-def is_flat(dirpath, import_name):
-    flat_src = Path(dirpath, import_name + '.py')
-    pkg_init_src = Path(dirpath, import_name) / '__init__.py'
-    if flat_src.exists() and pkg_init_src.exists():
-        raise ValueError(f'Both {import_name}.py and {import_name}/__init__.py'
-                         f' present in repository')
-    elif flat_src.exists():
-        return True
-    elif pkg_init_src.exists():
-        return False
-    else:
-        raise ValueError(f'Neither {import_name}.py nor'
-                         f' {import_name}/__init__.py present in repository')
-
 def get_commit_years(dirpath, include_now=True):
     years = set(map(
         int,
--- test/test_inspect_project.py
+++ test/test_inspect_project.py
@@ -7,47 +7,9 @@ import pytest
 from   pyrepo                 import util
 from   pyrepo.inspect_project import extract_requires, find_module, \
                                         get_commit_years, inspect_project, \
-                                        is_flat, parse_requirements
+                                        parse_requirements
 
 DATA_DIR = Path(__file__).with_name('data')
-
-@pytest.mark.parametrize(
-    'dirpath',
-    (DATA_DIR / 'is_flat' / 'flat').iterdir(),
-    ids=attrgetter("name"),
-)
-def test_is_flat_yes(dirpath):
-    assert is_flat(dirpath, "foobar")
-
-@pytest.mark.parametrize(
-    'dirpath',
-    (DATA_DIR / 'is_flat' / 'notflat').iterdir(),
-    ids=attrgetter("name"),
-)
-def test_is_flat_no(dirpath):
-    assert not is_flat(dirpath, "foobar")
-
-@pytest.mark.parametrize(
-    'dirpath',
-    (DATA_DIR / 'is_flat' / 'both').iterdir(),
-    ids=attrgetter("name"),
-)
-def test_is_flat_both(dirpath):
-    with pytest.raises(ValueError) as excinfo:
-        is_flat(dirpath, "foobar")
-    assert str(excinfo.value) \
-        == 'Both foobar.py and foobar/__init__.py present in repository'
-
-@pytest.mark.parametrize(
-    'dirpath',
-    (DATA_DIR / 'is_flat' / 'neither').iterdir(),
-    ids=attrgetter("name"),
-)
-def test_is_flat_neither(dirpath):
-    with pytest.raises(ValueError) as excinfo:
-        is_flat(dirpath, "foobar")
-    assert str(excinfo.value) \
-        == 'Neither foobar.py nor foobar/__init__.py present in repository'
 
 @pytest.mark.parametrize('gitoutput,result', [
     ('2019\n2019\n2018\n2016', [2016, 2018, 2019]),
